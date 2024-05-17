6448cfdcd3068afd39dbc980d5bfcae5e182bc71
Thaís Santos <86802712+thaissan@users.noreply.github.com>
2024-05-17 08:35:23 -0300

    iniciando projeto

---
+++ src/paginas/footer/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <>
+        <img src="https://www.petz.com.br/blog/wp-content/uploads/2019/07/vida-de-gato.jpg" alt="" />
+        </>
+    );
+}
+export default Footer;
+
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+        <>
+        <img src="https://www.petz.com.br/blog/wp-content/uploads/2019/07/vida-de-gato.jpg" alt="" />
+        </>
+    );
+}
+export default Home;
+
